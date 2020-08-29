3a4eb57427c238e2ce63ed6704a4b4b4f210df4c
ndgndg91 <ndgndg91@gmail.com>
2020-08-29 21:38:02 +0900

    react state

--- src/Application.js
+++ src/Application.js
@@ -4,37 +4,43 @@ class Application extends Component {
     constructor(props){
         super(props);
 
-    }
-
-    componentWillMount(props, state){
-
-    }
-
-    componentDidMount(props, state) {
-        console.log("Mounted with", props, state);
-    }
-
-    componentWillReceiveProps(props) {
-
-    }
-
-    componentWillUpdate(props, state) {
-        if(this.props.name !== props.name) {
-            //do something
+        this.state = {
+            count: 0,
+            overTen: false
         }
     }
 
+    handleClick = () => {
+        console.log('Clicked!');
+        this.setState({count: this.state.count + 1});
+    }
+
     componentDidUpdate(props, state) {
-    
+        console.log("Updated from", state, "to", this.state);
+        if (this.state.count > 10 && this.state.count !== state.count && !this.state.overTen) {
+            console.log("Updated over ten");
+            this.setState({overTen: true});
+        }
     }
  
     render(){
         let name = "Dong-Gil";
+        let {count} = this.state;
 
         return (
             <div>
                 <h1>Hello, {name}</h1>
-                <span>This</span>
+                <hr>
+                </hr>
+                <span>You Clicked the button {count} times</span>
+                <hr></hr>
+                {(this.state.overTen) ? 
+                    <span>Beat high score of 10!</span>
+                    : null
+                }
+                <span>
+                    <button onClick={(e) => this.handleClick()}>Click me</button>
+                </span>
             </div>
         );
     }
